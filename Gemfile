084c95d54ea3ba2704e56b8bbe04a0ac2212eaf0
Bianca Nistor <bnistor@optoro.com>
2017-12-12 09:12:02 -0500

    adding drivers and first test

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'selenium-webdriver', '~> 3.8.0'
+# gem 'page-object', '~> 2.2', '>= 2.2.4'
---
+++ test_bn.rb
@@ -0,0 +1,20 @@
+require "selenium-webdriver"
+
+#driver = Selenium::WebDriver.for :firefox
+
+Selenium::WebDriver::Chrome.driver_path="./drivers/chromedriver"
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--disable-popup-blocking')
+options.add_argument('--disable-translate')
+driver = Selenium::WebDriver.for :chrome, options: options
+
+driver.navigate.to "http://google.com"
+
+element = driver.find_element(name: 'q')
+element.send_keys "Hello WebDriver!"
+element.submit
+
+puts driver.title
+
+driver.quit
