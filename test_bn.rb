870cb7f3c4aa702de006cb82ace515d41eada358
Bianca Nistor <bnistor@optoro.com>
2017-12-12 10:09:11 -0500

    updated chromedriver version

--- test_bn.rb
+++ test_bn.rb
@@ -8,8 +8,7 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument('--disable-popup-blocking')
 options.add_argument('--disable-translate')
 driver = Selenium::WebDriver.for :chrome, options: options
-
-driver.navigate.to "http://google.com"
+driver.navigate.to "https://www.google.com"
 
 element = driver.find_element(name: 'q')
 element.send_keys "Hello WebDriver!"
